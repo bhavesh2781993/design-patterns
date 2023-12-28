95ffa4c11cf0288194c5403b3548dea7f7b5300f
Bhavesh <shah27bhavesh@gmail.com>
2023-12-28 10:32:52 +0530

    structural: composite

---
+++ src/main/java/structural/Composite.java
@@ -0,0 +1,57 @@
+package structural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Composite {
+    public static void main(String[] args) {
+        File border = new File();
+        border.filename = "Border";
+
+        Directory comedyMovies = new Directory();
+        comedyMovies.directoryName = "comedy_movies";
+        File gaddar = new File();
+        gaddar.filename = "Gaddar";
+        comedyMovies.add(gaddar);
+
+        Directory movies = new Directory();
+        movies.add(border);
+        movies.add(comedyMovies);
+
+        movies.ls();
+    }
+
+}
+
+interface FileSystem {
+    void ls();
+}
+
+class File implements FileSystem {
+    String filename;
+
+    @Override
+    public void ls() {
+        System.out.println("File: " + filename);
+    }
+}
+
+class Directory implements FileSystem {
+    String directoryName;
+    List<FileSystem> fileSystems;
+
+    public void add(FileSystem fileSystem) {
+        if (fileSystems == null) {
+            fileSystems = new ArrayList<>();
+        }
+        fileSystems.add(fileSystem);
+    }
+
+    @Override
+    public void ls() {
+        System.out.println("Directory: " + directoryName);
+        for(FileSystem fs: fileSystems) {
+            fs.ls();
+        }
+    }
+}
